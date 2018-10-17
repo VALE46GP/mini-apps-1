ad6f837334db6dff0cd67823310e5e55450da89e
Eric Callari <ericcallari1@gmail.com>
2018-10-16 19:38:09 -0700

    conversion renders to same page

--- challenge_2/convert-json-to-csv.js
+++ challenge_2/convert-json-to-csv.js
@@ -1,6 +1,27 @@
 
 var convertJSONtoCSV = function(jsonData) {
-  return 'CSVified(' + jsonData.json_data + ')';
+  // var obj = JSON.parse(jasonData);
+  // var csv = '';
+  //
+  // var currKeys = Object.keys(obj).map(v => v !== 'children');
+  //
+  // csv += currKeys.join(',') + '\n';
+  //
+  //
+  // if (obj.children && obj.children.length) {
+  //   obj.children.forEach(c => {
+  //
+  //   });
+  // }
+
+
+
+
+
+
+
+
+  return jsonData.json_data;
 };
 
 module.exports.convertJSONtoCSV = convertJSONtoCSV;
