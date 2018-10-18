559e1bbb92ab2c925bfc9a77749bc5297f3d69c4
Eric Callari <ericcallari1@gmail.com>
2018-10-18 11:22:49 -0700

    express server set up

---
+++ challenge_4/server/index.js
@@ -0,0 +1,12 @@
+var bodyParser = require('body-parser');
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.use(bodyParser.jason());
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+
+
+
+app.listen(3000, () => { console.log('listening on port ', 3000) });
